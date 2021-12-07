b226395e82b136795c1cabce8d6fa2deaf724e9a
Daniel da Silva Rodrigues <Dan.sil.rod.20@gmail.com>
2021-12-07 16:49:21 -0300

    Criei um novo diretorio para os scripts, acrescentei o addregister.js a ele, adicionei a tag referente a um script que ainda vou criar

---
+++ ex001/scripts/addregister.js
@@ -0,0 +1,22 @@
+const registerbtn = document.querySelector('input[type=button]');
+registerbtn.addEventListener('click', addregister);
+
+const element_name = document.getElementById('name');
+const element_date = document.getElementById('date');
+const element_price = document.getElementById('price');
+
+const allrecords = {};
+
+function addregister(){
+    let name = element_name.value.toUpperCase();
+    let register = {
+        date: element_date.value,
+        price: element_price.value
+    }
+
+    if (!(name in allrecords)){
+        allrecords[name] = [];
+    }
+
+    allrecords[name].push(register);
+}
