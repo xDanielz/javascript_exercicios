13c83d4ffd1496a4025d80f95494019b79ad66bc
Daniel da Silva Rodrigues <Dan.sil.rod.20@gmail.com>
2021-12-07 18:26:14 -0300

    Adicionei teste condicional para que não seja possivel adicionar registros incompletos

--- ex001/scripts/addregister.js
+++ ex001/scripts/addregister.js
@@ -4,9 +4,23 @@ registerbtn.addEventListener('click', addregister);
 const element_name = document.getElementById('name');
 const element_date = document.getElementById('date');
 const element_price = document.getElementById('price');
+
 var allrecords = {};
 
 function addregister(){
+    let proceed = true;
+    for(id of ['price', 'name', 'date']){
+        let element = eval(`element_${id}`)
+        if(!element.value){
+            proceed = false;
+            element.style.border = '1px solid red';
+            continue;
+        }
+        element.style.border = '1px solid black';
+    }
+
+    if(!proceed){return;}
+
     let name = element_name.value.toUpperCase();
     let register = {
         date: element_date.value,
