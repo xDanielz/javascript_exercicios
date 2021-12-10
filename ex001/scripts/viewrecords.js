c5841348dd7fa4b41c1b9f838f2438e138a12c7a
Daniel da Silva Rodrigues <Dan.sil.rod.20@gmail.com>
2021-12-10 19:43:08 -0300

    Feita alguns ajustes na data para ficar mais responsivel

--- ex001/scripts/viewrecords.js
+++ ex001/scripts/viewrecords.js
@@ -24,25 +24,34 @@ function viewrecords(){
         
         if(chosen != 'all' && chosen != n){continue;}
 
+        //Conteúdos respectivo ao registro que irão preencher o elementos td
         for(let i = 0; i < allrecords[n].length; i++){
+            //Formatando data
+            let unformateddate = allrecords[n][i].date;
+            let listdate = [unformateddate.slice(8,10), unformateddate.slice(5,7), unformateddate.slice(0,4)]
+            let date = listdate.join('/<wbr/>')
+
+            //Formatando Preço
+            let price = Number(allrecords[n][i].price).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+
+            //Lista com os dados a serem adicionados nas td
             let contents = [
                 n_id, 
                 n, 
-                allrecords[n][i].date, 
-                allrecords[n][i].price
+                date,
+                price
             ]
 
             tr = document.createElement('tr');
 
+            //Adicionando o conteúdo aos tds
             for(c in contents){
                 let td = document.createElement('td');
-                td.innerText = contents[c];
-                if(c == 3){
-                    td.className = 'tdprice';
-                    td.innerText = Number(contents[c]).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
-                }
+                td.innerHTML = contents[c];
+                if(c == 3){td.className = 'tdprice';}
                 tr.appendChild(td);
             }
+
             addbuttons(tr);//Adiciona os botões de deletar e alterar ao elemento tr;
 
             tbody_element.appendChild(tr);
