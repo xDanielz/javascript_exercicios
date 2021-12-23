d90b607bd92d242224df8c2ed1295269ebcf4ef7
Daniel da Silva Rodrigues <Dan.sil.rod.20@gmail.com>
2021-12-22 22:10:27 -0300

    Jogo quase finalizado

--- ex002/scripts/tictactoe.js
+++ ex002/scripts/tictactoe.js
@@ -149,11 +149,3 @@ function viewResult(result){
         element_result.innerHTML = txt;
     }
 }
-
-function syncDelay(milliseconds){
-    var start = new Date().getTime();
-    var end=0;
-    while( (end-start) < milliseconds){
-        end = new Date().getTime();
-    }
-}
