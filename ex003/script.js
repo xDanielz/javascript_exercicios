2c7e182dd507f9c3b934d53c55526a8abf630c27
Daniel <dan.sil.rod.20@gmail.com>
2022-04-26 17:10:49 -0300

    Arquivo index.html e script.js atualizados, para que seja exibido o total (preço sugerido menos - combústivel)

--- ex003/script.js
+++ ex003/script.js
@@ -1,8 +1,9 @@
 const element_km = document.getElementById('km');
 const element_fuel = document.getElementById('fuel');
 const element_price = document.getElementById('price');
-const element_result = document.getElementById('result');
 const element_average = document.getElementById('average');
+
+const element_result = document.getElementById('result');
 const element_btn = document.querySelector('input[type=button]');
 const element_clean = document.querySelector('input[value=Limpar]');
 const input_elements = document.querySelectorAll('input[type=number');
@@ -11,6 +12,7 @@ element_btn.addEventListener('click', () => {
 
     const element_fuelcost = document.getElementById('fuelcost');
     const element_cost = document.getElementById('cost');
+    const element_total = document.getElementById('total');
 
     let km = element_km.value;
     let fuelprice = element_fuel.value;
@@ -25,10 +27,15 @@ element_btn.addEventListener('click', () => {
     element_clean.style.display = 'block';
     document.getElementById('result').style.display = 'block';
 
-    let fuel = (km * 2/average * fuelprice).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
-    let cost = (km*2*price).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});;
+    let fuel = km * 2/average * fuelprice
+    let cost = km*2*price
+    let total = (cost - fuel).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'}); 
+    fuel = fuel.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+    cost = cost.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+
     element_fuelcost.innerHTML = `<strong>${fuel}</strong>`;
     element_cost.innerHTML = `<strong>${cost}</strong>`;
+    element_total.innerHTML = `<strong>${total}</strong>`;
 });
 
 element_clean.addEventListener('click', () =>{
